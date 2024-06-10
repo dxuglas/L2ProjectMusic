827fbb580357f66891dda57bff0b1ed40ff0fb93
dxuglas <76638123+dxuglas@users.noreply.github.com>
2024-06-11 10:50:14 +1200

    volume and current song setup

--- ui/media_interface.py
+++ ui/media_interface.py
@@ -3,7 +3,9 @@ from PyQt6.QtWidgets import (
   QPushButton, 
   QHBoxLayout, 
   QVBoxLayout, 
-  QSlider
+  QGridLayout,
+  QSlider,
+  QLabel
 )
 from PyQt6.QtCore import (
   QSize, 
@@ -23,7 +25,7 @@ class MediaInterface(QFrame):
     self.setFixedHeight(int(self.parent.screen_size[0] * 0.046))
 
     self.layout = QHBoxLayout()
-    self.layout.setContentsMargins(0, 0, 0, 0)
+    self.layout.setContentsMargins(10, 0, 10, 0)
     self.setLayout(self.layout)
 
     self.controls_layout = QVBoxLayout()
@@ -39,6 +41,7 @@ class MediaInterface(QFrame):
     
     self.controls_container = QFrame()
     self.controls_container.setLayout(self.controls_layout)
+    self.layout.addWidget(SongInfo(self))
     self.layout.addWidget(self.controls_container, alignment=Qt.AlignmentFlag.AlignCenter)
     self.layout.addWidget(VolumeControls(self), alignment=Qt.AlignmentFlag.AlignRight)
 
@@ -108,6 +111,35 @@ class MediaControls(QFrame):
       self.pause_play_state = True
 
       
+class SongInfo(QFrame):
+  def __init__(self, parent) -> None:
+    super().__init__(parent) 
+
+    self.setObjectName("SongInfo")
+
+    self.size = int(parent.parent.screen_size[0] * 0.01)
+
+    self.layout = QGridLayout()
+    self.setLayout(self.layout)
+
+    self.art = QPushButton(objectName = "art",
+                           icon = QIcon(r"ui\assets\placeholder.svg"),
+                           flat = True,  
+                           iconSize = QSize(self.size*2, self.size*2))
+    self.art.setFixedSize(QSize(self.size*3, self.size*3))
+    self.layout.addWidget(self.art, 0, 0)
+
+    self.name_layout = QVBoxLayout()
+    self.name_layout.setContentsMargins(5, 10, 0, 10)
+    self.song_name = QLabel("Song Name", objectName = "song_name")
+    self.artist_name = QLabel("Artist Name", objectName = "artist_name")
+
+    self.name_layout.addWidget(self.song_name)
+    self.name_layout.addWidget(self.artist_name)
+
+    self.layout.addLayout(self.name_layout, 0, 1)
+
+
 class ProgressBar(QFrame):
   def __init__(self, parent) -> None:
     super().__init__(parent)
@@ -121,15 +153,19 @@ class ProgressBar(QFrame):
     self.layout.setContentsMargins(0, 0, 0, 0)
     self.setLayout(self.layout)
 
-    self.bar = QSlider(Qt.Orientation.Horizontal, objectName="bar")
+    self.bar = QSlider(Qt.Orientation.Horizontal, objectName="time_bar")
     self.bar.setRange(0, 100)
     self.layout.addWidget(self.bar)
+
 
 class VolumeControls(QFrame):
   def __init__ (self, parent) -> None:
     super().__init__(parent)
 
     self.setObjectName("VolumeControls")
+
+    self.volume_state = 2
+    self.volume = 100
 
     self.layout = QHBoxLayout()
     self.layout.setContentsMargins(0, 0, 0, 0)
@@ -142,5 +178,41 @@ class VolumeControls(QFrame):
                                    size = QSize(self.btn_size, self.btn_size), 
                                    iconSize = QSize(self.btn_size, 
                                                     self.btn_size))
+    self.mute_btn.clicked.connect(self.mute_btn_pressed)
+    self.mute_btn.state = False
 
     self.layout.addWidget(self.mute_btn)
+
+    self.bar = QSlider(Qt.Orientation.Horizontal, objectName="volume_bar")
+    self.bar.setRange(0, 100)
+    self.bar.setValue(100)
+    self.bar.setFixedHeight(int(self.btn_size / 3))
+    self.bar.valueChanged.connect(self.volume_changed)
+
+    self.layout.addWidget(self.bar)
+
+  def mute_btn_pressed(self):
+    if self.mute_btn.state:
+      self.mute_btn.setIcon(QIcon(fr"ui\assets\mute_off_{self.volume_state}.svg"))
+      self.mute_btn.state = False
+      self.volume = self.bar.value()
+      self.bar.setEnabled(True)
+    else:
+      self.mute_btn.setIcon(QIcon(r"ui\assets\mute_on.svg"))
+      self.mute_btn.state = True
+      self.bar.setEnabled(False)
+      self.volume = 0
+
+  def volume_changed(self):
+    if self.bar.value() > 66:
+      self.mute_btn.setIcon(QIcon(r"ui\assets\mute_off_2.svg"))
+      self.volume_state = 2
+    elif self.bar.value() > 33:
+      self.mute_btn.setIcon(QIcon(r"ui\assets\mute_off_1.svg"))
+      self.volume_state = 1
+    else:
+      self.mute_btn.setIcon(QIcon(r"ui\assets\mute_off_0.svg"))
+      self.volume_state = 0
+    
+    self.volume = self.bar.value()
+    print(self.volume)
